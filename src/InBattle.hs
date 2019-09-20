9daafcd3660c793756ae5f4623508b951c7e1f8f
GoodSeat <GoodSeat@hotmail.co.jp>
2019-09-20 11:45:01 +0900

    refactor InBattle.hs

--- src/InBattle.hs
+++ src/InBattle.hs
@@ -5,6 +5,7 @@ import Data.List
 import Data.Maybe
 import qualified Data.Map as Map
 import Control.Monad
+import Control.Monad.State
 import GameAuto
 import Utils
 import World
@@ -15,7 +16,7 @@ import qualified Spells as Spell
 import qualified Items as Item
 
 data BattleAction = ByParties Character.ID Action
-                  | ByEnemies Enemy.Instance Enemy.Action
+                  | ByEnemies Int Enemy.Instance Enemy.Action
     deriving (Show)
 
 data Action = Fight Int
@@ -37,7 +38,7 @@ data Condition = Condition {
 
 -- ==========================================================================
 decideEnemyInstance :: Enemy.ID -> GameState [[Enemy.Instance]]
-decideEnemyInstance = decideEnemyInstance' 1
+decideEnemyInstance e = decideEnemyInstance' 1 e >>= tryDetermineEnemies
   where
     decideEnemyInstance' :: Int -> Enemy.ID -> GameState [[Enemy.Instance]]
     decideEnemyInstance' n eid = if n > 4 then return [] else do
@@ -46,27 +47,29 @@ decideEnemyInstance = decideEnemyInstance' 1
         withBack <- happens $ Enemy.withBackProb def
         bl  <- if withBack then decideEnemyInstance' (n + 1) =<< (Enemy.ID <$> eval (Enemy.backEnemyID def))
                            else return []
-        det <- identifyEnemies
-        el  <- createEnemyInstances n eid det True
+        el  <- createEnemyInstances n eid True
         return $ el : bl
-
-identifyEnemies :: GameState Bool
-identifyEnemies = (<=) <$> randomNext 1 10 <*> (length . party <$> world)
 
 createEnemyInstances :: Int          -- ^ num of create instaces.
                      -> Enemy.ID     -- ^ target id of enemy.
-                     -> Bool         -- ^ enemies are determined or not.
                      -> Bool         -- ^ maybe drop item or not.
                      -> GameState [Enemy.Instance]
-createEnemyInstances 0 _ _ _            = return []
-createEnemyInstances n eid det dropItem = do
+createEnemyInstances 0 _ _              = return []
+createEnemyInstances n eid dropItem = do
     def <- enemyOf eid
-    es  <- createEnemyInstances (n - 1) eid det False
+    es  <- createEnemyInstances (n - 1) eid False
     mhp <- eval $ Enemy.maxhp def
     let e = Enemy.Instance {
-      Enemy.id = eid, Enemy.determined = det, Enemy.hp = mhp, Enemy.statusErrors = [], Enemy.maybeDropItem = dropItem
+      Enemy.id = eid, Enemy.determined = False, Enemy.hp = mhp, Enemy.statusErrors = [], Enemy.maybeDropItem = dropItem
     }
     return $ e : es
+
+tryDetermineEnemies :: [[Enemy.Instance]] -> GameState [[Enemy.Instance]]
+tryDetermineEnemies = sequence . fmap tryDetermineGroup
+  where
+    tryDetermineGroup es = do
+        determine <- (<=) <$> randomNext 1 10 <*> (length . party <$> world)
+        return $ if determine then fmap (\e -> e { Enemy.determined = True }) es else es
 
 -- ==========================================================================
 startBattle :: Enemy.ID             -- ^ encounted enemy.
@@ -102,7 +105,7 @@ selectBattleCommand i cmds con = Auto $ do
                     , selectBattleCommand (i + 1) ((cid, Parry) : cmds) con
                     , Character.Parry `elem` cs)
                    ,( Key "r"
-                    , afterRun con -- TODO:implement possible of fail to run.
+                    , events [Message $ Character.name c ++ " flees."] (afterRun con) -- TODO:implement possible of fail to run.
                     , Character.Run `elem` cs)
                     ]
   where
@@ -137,26 +140,21 @@ confirmBattle cmds con = Auto $ select (BattleCommand "Are you OK?\n\nF)ight\nT)
 
 nextTurn :: Condition -> GameAuto
 nextTurn con = Auto $ do
-    ess  <- lastEnemies
     con' <- updateCondition con
-    let ess'  = filter (\e -> Enemy.hp e > 0) <$> ess -- remove dead enemy.
-        ess'' = filter (not . null) ess'              -- remove null line.
-    ess''' <- sequence $ tryDetermineGroup <$> ess''
-    moveToBattle ess'''
-    if null ess''' then run $ wonBattle con'
-    else run $ selectBattleCommand 1 [] con'
-
-tryDetermineGroup :: [Enemy.Instance] -> GameState [Enemy.Instance]
-tryDetermineGroup es = do
-    determine <- identifyEnemies
-    return $ if determine then fmap (\e -> e { Enemy.determined = True }) es else es
+    ess  <- execState (do
+                modify $ fmap $ filter (\e -> Enemy.hp e > 0) -- remove dead or fleed enemy.
+                modify $ filter (not . null)                  -- remove null line.
+            ) <$> lastEnemies
+    ess' <- tryDetermineEnemies ess
+    moveToBattle ess'
+    run $ (if null ess' then wonBattle else selectBattleCommand 1 []) con'
 
 updateCondition :: Condition -> GameState Condition
 updateCondition con = do
     ess   <- lastEnemies
     drops <- forM (concat ess) (\e -> do
         edef <- enemyOf $ Enemy.id e
-        if Enemy.hp e > 0 then return (0, 0, [])
+        if notElem Character.Dead $ Enemy.statusErrors e then return (0, 0, [])
         else (,,) <$> eval (Enemy.dropGold edef) <*> pure (Enemy.exp edef) <*> pure [])
     let (g, exp, is) = foldl' (\(g1, e1, is1) (g2, e2, is2) -> (g1 + g2, e1 + e2, is1 ++ is2)) (0, 0, []) drops
     return $ con { dropGold = dropGold con + g, gotExps = gotExps con + exp, dropItems = dropItems con ++ is }
@@ -169,6 +167,7 @@ wonBattle con = Auto $ do
         es = Just (Message $ "Each survivor got " ++ show e ++ " E.P.")
            : (if g > 0 then Just (Message $ "Each survivor got " ++ show g ++ " G.P.") else Nothing)
            : []
+    forM_ ps $ flip updateCharacterWith (\c -> c { Character.exp = Character.exp c + e, Character.gold = Character.gold c + g })
     run $ events (catMaybes es) (afterWin con)
 
 
@@ -190,25 +189,29 @@ act (ByParties id a) = case a of
     Fight l -> do
       ses <- Character.statusErrors <$> characterOf id
       enm <- filter (\e -> Enemy.hp e > 0) <$> ((!!) <$> lastEnemies <*> pure (l - 1))
-      if   any (`elem` Character.cantFightStatus) ses then return []
-      else if null enm then return []
+      if   any (`elem` Character.cantFightStatus) ses || null enm then return []
       else do
         c <- characterOf id
         e <- randomIn enm
         (h, d) <- fightDamage l c e
-        let e' = e { Enemy.hp = Enemy.hp e - d }
+        let hp' = Enemy.hp e - d
+            st' = Enemy.statusErrors e ++ [Character.Dead | hp' <= 0]
+            e'  = e { Enemy.hp = hp', Enemy.statusErrors = st' }
         updateEnemy l e (const e')
         fmap Message <$> fightMessage c e' (h, d)
     Parry   -> return []
     Run     -> return []
-act (ByEnemies e a) = case a of
+act (ByEnemies l e a) = case a of
     Enemy.Fight n d t effs -> return []
-    Enemy.Run              -> return []
+    Enemy.Run              -> do
+        en   <- enemyNameOf e
+        updateEnemy l e $ const e { Enemy.hp = 0}
+        return [Message $ en ++ " flees."]
 
 
 fightDamage :: Int -> Character.Character -> Enemy.Instance -> GameState (Int, Int)
 fightDamage l c e = do
-    edef     <- enemyOf $ Enemy.id e
+    edef <- enemyOf $ Enemy.id e
     let tryCountF = parse' "lv/5 + 1" -- TODO:
         jobBonusF = parse' "lv/3 + 2" -- TODO:
         damageF   = parse' "2d2"      -- TODO:from wepon.
@@ -224,7 +227,7 @@ fightDamage l c e = do
     rs <- replicateM tryCount $ do
         hit <- (<=) <$> randomNext 1 20 <*> pure atSkill
         dam <- (+) <$> evalWith m damageF <*> pure (max 0 strBonus)
-        let dam' = if (not . null $ Enemy.statusErrors e) then dam * 2 else dam
+        let dam' = if not . null $ Enemy.statusErrors e then dam * 2 else dam
         return $ if hit then (1, dam') else (0, 0)
     return $ foldl' (\(h1, d1) (h2, d2) -> (h1 + h2, d1 + d2)) (0, 0) rs
   where
@@ -233,14 +236,20 @@ fightDamage l c e = do
                                 ,("str", Character.strength . Character.param $ c)
                                 ]
 
+enemyNameOf :: Enemy.Instance -> GameState String
+enemyNameOf e = nameOf <$> enemyOf (Enemy.id e)
+  where
+    nameOf = if Enemy.determined e then Enemy.name else Enemy.nameUndetermined
+
+
 fightMessage :: Character.Character -> Enemy.Instance -> (Int, Int) -> GameState [String]
 fightMessage c e (h, d) = do
-    en <- (if Enemy.determined e then Enemy.name else Enemy.nameUndetermined) <$> enemyOf (Enemy.id e)
+    en <- enemyNameOf e
     v  <- randomIn vs
     let m1 = Character.name c ++ " " ++ v ++ "\n " ++ en ++ ".\n"
     let m2 = if h == 0 then " and misses." else " and hits " ++ show h ++ " times for " ++ show d ++ ".\n"
     let m3 = if Enemy.hp e <= 0 then en ++ " is killed." else ""
-    return $ (m1 ++ m2) : if null m3 then [] else (m1 ++ m3) : []
+    return $ (m1 ++ m2) : if null m3 then [] else [m1 ++ m3]
   where
     vs = ["leaps at", "attempts to slice", "thrusts violently at", "tries to ram", "tries to bash", "charges at", "tries to slash"]
 
@@ -253,8 +262,10 @@ fightMessage c e (h, d) = do
 determineActions :: [(Character.ID, Action)]
                  -> GameState [BattleAction]
 determineActions cmds = do
-    pcs <- sequence $ toPair <$> cmds
-    ecs <- sequence =<< map toEnemyAction <$> (concat <$> lastEnemies)
+    pcs  <- sequence $ toPair <$> cmds
+    elss <- zip [1..] <$> lastEnemies
+    let els = concat $ (\(l, es) -> zip (repeat l) es) <$> elss
+    ecs  <- sequence $ toEnemyAction <$> els
     return $ snd <$> sortOn fst (pcs ++ ecs)
   where
     toPair :: (Character.ID, Action) -> GameState (Int, BattleAction)
@@ -264,12 +275,12 @@ determineActions cmds = do
         return (key, ByParties id act)
 
 
-toEnemyAction :: Enemy.Instance -> GameState (Int, BattleAction)
-toEnemyAction ei = do
+toEnemyAction :: (Int, Enemy.Instance) -> GameState (Int, BattleAction)
+toEnemyAction (l, ei) = do
     def <- enemyOf $ Enemy.id ei
     key <- agiBonus $ Character.agility . Enemy.param $ def
     act <- randomIn $ Enemy.actions def
-    return (key, ByEnemies ei act)
+    return (key, ByEnemies l ei act)
 
 
 agiBonus :: Int -> GameState Int
